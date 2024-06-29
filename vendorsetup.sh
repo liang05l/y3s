bfb897900aea6ab1f1e3bf3667e3f9c0d4773c0c
liang05l <3034586416@qq.com>
2024-06-30 00:06:37 +0800

    d1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_2044-user
-add_lunch_combo omni_2044-userdebug
-add_lunch_combo omni_2044-eng
+COMMON_LUNCH_CHOICES omni_2044-user
+COMMON_LUNCH_CHOICES omni_2044-userdebug
+COMMON_LUNCH_CHOICES omni_2044-eng
